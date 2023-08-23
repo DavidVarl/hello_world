4a25a3bbe09f9ff61d0c7a05bd353b255cbd2c0f
david0011 <4106938-david0011@users.noreply.replit.com>
2023-08-23 20:29:05 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,74 @@
+//this functon outputs hello linoln 
+function T() {
+  console.log("TTTTTTTTTTTT")
+  console.log("     T      ")
+  console.log("     T      ")
+  console.log("     T      ")
+  console.log("     T      ")
+  console.log("     T      ")
+  console.log("     T      ")
+}
+
+function I(){
+  console.log("IIIIIIIIIIII")
+  console.log("     I      ")
+  console.log("     I      ")
+  console.log("     I      ")
+  console.log("     I      ")
+  console.log("     I      ")
+  console.log("     I      ")
+  console.log("     I      ")
+  console.log("IIIIIIIIIIII")
+} 
+function E() {
+  console.log("EEEEEEEEE")
+  console.log("E")
+  console.log("E")
+  console.log("EEEEEEEEE")
+  console.log("E")
+  console.log("E")
+  console.log("EEEEEEEEE")
+  console.log();
+}
+function G() {
+  console.log("        GGGGGG ")
+  console.log("      GGGGGG")
+  console.log("    GGGGG  ")
+  console.log("  GGGGGG  ")
+  console.log(" GGGGG       GGGGGGG")
+  console.log(" GGGGG         GGGGG")
+  console.log(" GGGGGG         GGGG")
+  console.log("  GGGGGGG     GGGGG")
+  console.log("     GGGGGGGGGGGG")
+  console.log("      GGGGGGGGGG")
+  
+}
+function R() {
+ console.log("  RRRRRRRR") 
+ console.log("RR       RR") 
+ console.log("RR       RR") 
+ console.log("RRRRRRRRRR") 
+ console.log("RRR       RRR") 
+ console.log("RRR       RRR") 
+ console.log("RRR       RR") 
+ console.log(); 
+}
+function S() {
+  console.log("    SSSS   ")
+  console.log("SSSS   ")
+  console.log("  SSSS   ")
+  console.log("    SSSS   ")
+  console.log("       SSSS   ")
+  console.log("     SSSS   ")
+  console.log("   SSSS   ")
+  console.log("SSSS   ")
+  console.log()
+}
+for (var i=0; i < 20; i ++) {
+  T();
+  I();
+  G();
+  E();
+  R();
+  S();
+}
